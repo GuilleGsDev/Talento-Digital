8e1f731521e75a2cb69e67420978f88463811aff
Guillermo Gonzalez <guillermo.gonzalez.s01@mail.pucv.cl>
2026-06-17 21:23:37 -0400

    docs: agregar documentacion requerida a los archivos css y javascript

--- Unidad-1/Viajes-Chile-page/assets/js/script.js
+++ Unidad-1/Viajes-Chile-page/assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+    // 1. Efecto de cambio de color en el Navbar al hacer scroll hacia abajo
     $(window).scroll(function () {
         if ($(this).scrollTop() > 50) {
             $('.navbar').addClass('navbar-scrolled');
@@ -7,7 +7,7 @@ $(document).ready(function () {
             $('.navbar').removeClass('navbar-scrolled');
         }
     });
-
+    // 2. Smooth Scroll: Animación de desplazamiento suave para los enlaces del menú
     $("a.nav-link").on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
@@ -20,7 +20,7 @@ $(document).ready(function () {
             });
         }
     });
-
+    // 3. Inicialización de componentes Bootstrap JS: Activa los tooltips en la página
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
